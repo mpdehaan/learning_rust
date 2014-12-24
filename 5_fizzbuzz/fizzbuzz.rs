f3d66b39a9dc991499640b157b8139ba093c5acd
Michael DeHaan <michael.dehaan@gmail.com>
2014-12-24 13:15:41 -0500

    More tests, fix makefile target ambiguity

--- 5_fizzbuzz/fizzbuzz.rs
+++ 5_fizzbuzz/fizzbuzz.rs
@@ -8,5 +8,8 @@ fn test_div_by_three() {
    if div_by_three(1) {
        panic!("One is not divisible by three")
    }
+   if !div_by_three(3) {
+       panic!("Three is divisible by three")
+   }
 }
 
