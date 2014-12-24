6e9654e84c7ddf66d6cac900e21a1cbeea81ec44
Michael DeHaan <michael.dehaan@gmail.com>
2014-12-24 13:55:45 -0500

    Fixing the channel example WRT latest nightly.

--- 6_tasks/tasks.rs
+++ 6_tasks/tasks.rs
@@ -4,10 +4,18 @@ fn main() {
     //    println!("Hello");
     // }
 
-    for num in range(0u, 500) {
-        spawn(move || 
-            println!("Hello")
-        )
-    }
+    // for num in range(0u, 500) {
+    //    spawn(move || 
+    //        println!("Hello")
+    //    )
+    // }
+
+    let (chan, port) = channel();
+
+    spawn(move ||
+        chan.send(10u)
+    );
+
+    println!("{}", port.recv().to_string());
 
 }
