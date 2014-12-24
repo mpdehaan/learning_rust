7ae0da0f493b01b3202685e27a675506152b3357
Michael DeHaan <michael.dehaan@gmail.com>
2014-12-24 13:30:51 -0500

    Following along with RustForRubyists tutorial by adding other examples, though this algorithm is suboptimal :)

--- 5_fizzbuzz/fizzbuzz.rs
+++ 5_fizzbuzz/fizzbuzz.rs
@@ -3,6 +3,14 @@ fn div_by_three(num: int) -> bool {
    num % 3 == 0
 }
 
+fn div_by_five(num: int) -> bool {
+   num % 5 == 0
+}
+
+fn div_by_fifteen(num: int) -> bool {
+   num % 5 == 0
+}
+
 #[test]
 fn test_div_by_three() {
    if div_by_three(1) {
@@ -10,6 +18,32 @@ fn test_div_by_three() {
    }
    if !div_by_three(3) {
        panic!("Three is divisible by three")
+   }
+}
+
+#[test]
+fn test_div_by_five() {
+   if div_by_five(1) {
+      panic!("1 is not divisible by five")
+   }
+   if !div_by_five(5) {
+      panic!("5 is divisible by five")
+   }
+   if !div_by_five(15) {
+      panic!("15 is divisible by five")
+   }
+}
+
+#[test]
+fn test_div_by_fifteen() {
+   if div_by_fifteen(1) {
+      panic!("1 is not divisible by fifteen")
+   }
+   if !div_by_fifteen(15) {
+      panic!("15 is not divisible by fifteen")
+   }
+   if !div_by_fifteen(15) {
+      panic!("Raargh, these tests are silly but I'm following along with the tutorial anyway")
    }
 }
 
