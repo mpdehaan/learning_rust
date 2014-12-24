aa3fb1ea4c65fdfcc60b9a37bcc3acca877c43b8
Michael DeHaan <michael.dehaan@gmail.com>
2014-12-24 13:52:50 -0500

    Spawning subtasks.  The "move ||" bit makes an unamed closure compared with a named function.

--- 6_tasks/tasks.rs
+++ 6_tasks/tasks.rs
@@ -1,5 +1,13 @@
 fn main() {
+
+    // for num in range(0u, 500) {
+    //    println!("Hello");
+    // }
+
     for num in range(0u, 500) {
-        println!("Hello");
+        spawn(move || 
+            println!("Hello")
+        )
     }
+
 }
