f62af36e1df4118093c2e54c56ab66600789350a
Michael DeHaan <michael.dehaan@gmail.com>
2014-12-24 15:50:32 -0500

    Type casting examples

--- 2_rust_by_example/5_types/main.rs
+++ 2_rust_by_example/5_types/main.rs
@@ -18,4 +18,20 @@ fn main() {
     // bool either true or false
     // and the unit type (), whose only value is also ()
 
+    // ===========================
+    // casting is available but must be explicit
+
+    let decimal = 65.4321_f32;
+
+    // Error! No implicit conversion
+    // let integer: u8 = decimal;
+    // FIXME ^ Comment out this line
+
+    // Explicit conversion
+    let integer = decimal as u8;
+    let character = integer as char;
+
+    println!("Casting: {} -> {} -> {}", decimal, integer, character);
+
+
 }
