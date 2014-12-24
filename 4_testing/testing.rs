7de440b3b8b0e84eea2b4f7b632cf20307e4626a
Michael DeHaan <michael.dehaan@gmail.com>
2014-12-24 13:12:39 -0500

    WIP.

---
+++ 4_testing/testing.rs
@@ -0,0 +1,16 @@
+
+#[test]
+fn this_tests_code() {
+    println!("")
+}
+
+#[test]
+fn this_tests_more_code() {
+    // this used to be named fail! in earlier versions of rust
+    panic!("Boom!")
+}
+
+fn main() {
+    println!("Yo!")
+}
+
