1447081de7739f12c33a79c753e4c71d03636397
Michael DeHaan <michael.dehaan@gmail.com>
2014-12-24 14:45:09 -0500

    A note about Box.

--- 7_pointers/pointers.rs
+++ 7_pointers/pointers.rs
@@ -1,3 +1,6 @@
+// A box, with type signature Box<T>, is a smart pointer to a heap allocated value of type T
+// source: http://rustbyexample.com/box.html
+
 fn plus_one(x: Box<int>) -> int {
    *x + 1
 }
