8c1515acafae587d4537c3cdffacb57460553734
Michael DeHaan <michael.dehaan@gmail.com>
2014-12-24 15:04:40 -0500

    Basic pushing of slices onto vectors.

--- 9_vectors/vectors.rs
+++ 9_vectors/vectors.rs
@@ -2,7 +2,14 @@ fn main() {
 
     let things  = vec!(1i, 2i, 3i);
 
-    println!("The third favorite number is {}.", things[2])
+    println!("The third favorite number is {}.", things[2]);
+
+    let mut another_vector = vec!(4i);
+
+    another_vector.push_all(&[1, 2, 3]);
+
+    println!("The second number is {}.", another_vector[1])
+
 
 }
 
