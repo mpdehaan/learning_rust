0ca8e860d27c1568969dbcf090420a485b4e5278
Michael DeHaan <michael.dehaan@gmail.com>
2014-12-24 17:07:14 -0500

    Not quite sure how to pass values to cfgspecs.  Shrug.

--- 2_rust_by_example/14_conditional_compile/main.rs
+++ 2_rust_by_example/14_conditional_compile/main.rs
@@ -11,9 +11,13 @@ fn are_you_on_linux() {
     println!("You are *not* running linux!")
 }
 
-#[cfg(some_condition)]
+#[cfg(some_condition = "foo")]
 fn conditional_function() {
     println!("condition met!")
+}
+#[cfg(not(some_condition = "foo"))]
+fn conditional_function() {
+    println!("condition not met!")
 }
 
 fn main() {
