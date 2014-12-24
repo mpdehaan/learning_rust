82c1e1a83a43980e122422aaf8fd35ec4413161b
Michael DeHaan <michael.dehaan@gmail.com>
2014-12-24 15:39:36 -0500

    Mutable variable example.

---
+++ 2_rust_by_example/4_variables/mutability.rs
@@ -0,0 +1,20 @@
+
+fn main() {
+
+    // variables are immutable by default
+    let _immutable_variable = 1i;
+    let mut mutable_variable = 1i;
+
+    println!("Before mutation: {}", mutable_variable);
+
+    // Ok
+    mutable_variable += 1;
+
+    println!("After mutation: {}", mutable_variable);
+
+    // Error!
+    // _immutable_variable += 1;
+    // FIXME ^ Comment out this line
+
+}
+
